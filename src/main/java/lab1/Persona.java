c8e35228fc40e8add3262abece0848ec7d4a1a66
Julio Carrion <a20161448@pucp.edu.pe>
2020-05-09 11:11:59 -0500

    julio

---
+++ src/main/java/lab1/Persona.java
@@ -0,0 +1,67 @@
+package lab1;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String genero;
+    private String fecha_nacimiento;
+    private Double peso;
+    private String direccion_ip;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public String getDireccion_ip() {
+        return direccion_ip;
+    }
+
+    public void setDireccion_ip(String direccion_ip) {
+        this.direccion_ip = direccion_ip;
+    }
+}
---
+++ src/main/java/lab1/pregunta2.java
@@ -0,0 +1,59 @@
+package lab1;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.xml.internal.bind.v2.model.core.TypeRef;
+import jdk.internal.org.objectweb.asm.TypeReference;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class pregunta2 {
+
+    public void listraPersonas(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        ListaPErsonas personas2 = objectMapper.readValue(Paths.get(this.getClass().getResource("/personas.json").getFile()), ListaPErsonas.class);
+        for (Persona personas: personas2.listaper) {
+            String impreso = personas.getNombre() + " " + personas.getApellido()+ " " + personas.getGenero()+ " " + personas.getEmail()+ " "
+                    + personas.getFecha_nacimiento() + " " +  personas.getPeso() + " " + personas.getDireccion_ip();
+            System.out.println(impreso);
+        }
+    }
+    public void crearPersonas(){
+        Scanner sc = new Scanner(System.in);
+        Persona persona = new Persona();
+        System.out.print("Nombre: ");
+        persona.setNombre(sc.nextLine());
+        System.out.print("Apellido: ");
+        persona.setApellido(sc.nextLine());
+        System.out.print("Email: ");
+        persona.setEmail(sc.nextLine());
+        System.out.print("Peso: ");
+        persona.setPeso(Double.parseDouble(sc.nextLine()));
+        System.out.print("Fecha de Nacimiento: ");
+        persona.setFecha_nacimiento(sc.nextLine());
+        System.out.print("Genero: F o M? ");
+        if (sc.nextLine().equalsIgnoreCase("f")){
+            persona.setGenero("F");
+        }else{
+            persona.setGenero("M");
+        }
+        System.out.print("Direccion IP: ");
+        persona.setDireccion_ip(sc.nextLine());
+
+
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            mapper.writeValue(Paths.get("personas.json").toFile(), persona);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+
+}
