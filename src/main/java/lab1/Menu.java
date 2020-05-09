0ba9f0e45466973f53a26bab9fe92d6cec09d73c
JP VEGA <the-k@DESKTOP-TIA7P1E>
2020-05-09 10:25:10 -0500

    CREE LA CLASE ABSTRACT MENU

---
+++ src/main/java/lab1/Menu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author the-k
+ */
+public abstract class Menu {
+    public String menu;
+    public int opciones;
+    public int acc;
+    public Menu(){
+        
+    }
+
+    /**
+     *
+     */
+    public abstract void mostrarMenu();
+    public abstract void Accion(int acc);
+}
