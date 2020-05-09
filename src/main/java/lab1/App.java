8cc40c566666f1fd4c73a0f0de13c294d3c8c811
JP VEGA <the-k@DESKTOP-TIA7P1E>
2020-05-09 10:45:13 -0500

    estoy creando elmenu principal

--- src/main/java/lab1/App.java
+++ src/main/java/lab1/App.java
@@ -1,7 +1,30 @@
 package lab1;
 
-public class App {
+import java.util.Scanner;
+
+public class App  {
     public static void main(String[] args) {
     
+        int entrada=0;
+        double num1,num2;
+     
+        
+        do{
+            System.out.println("MENU PRINCIPAL");
+            System.out.println("INGRESE UNAOPCION");
+            
+            System.out.println("1.-APP_1");
+            System.out.println("2.-APP_2");
+            System.out.println("3.-SALI");
+               Scanner scanner=new Scanner(System.in);
+               while(entrada!=1&&entrada!=){
+                   
+               }
+            
+        }while(entrada!=3);
+        
+        
+        
+        
     }
 }
